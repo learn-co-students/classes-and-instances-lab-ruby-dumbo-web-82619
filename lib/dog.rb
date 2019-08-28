f5cee2b94d2ce14ea41247c21ea5a63f3c609a2b
github username <github email address>
2019-08-28 09:29:35 -0400

    done

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+    "Man's Best Friend"
+end
+
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
+
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+    "Human Being"
+end
+
+adele_goldberg = Person.new
+
+alan_key = Person.new
